c9b7bf7f1fcd77f3ef8b1808f5f7e08a457805fc
Jared Barboza <jared.m.barboza@gmail.com>
2014-10-18 21:51:59 -0400

    add benchmark to test script

--- test/harness.js
+++ test/harness.js
@@ -1,11 +1,16 @@
 // really simple test harness for our hackerrank scripts
 
+var eol = require('os').EOL;
 var fs = require('fs');
 var stdin = require('mock-stdin').stdin();
 var txt = fs.readFileSync(process.argv[3], 'utf8');
 
 require('../' + process.argv[2]); // require from the root
 
+var now = Date.now();
 stdin.send(txt.split('\n'));
 
 stdin.send(null);
+
+process.stdout.write(eol);
+process.stdout.write(eol + "took " + (Date.now() - now) + "ms");
